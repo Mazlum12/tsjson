3446ca7528bf3fdd2d4cf856aadf7ea4d54dcdcd
Mazlum12 <mazlum.cagriby@docc.techstarter.de>
2023-01-12 15:24:26 +0100

    Sorry

--- test.py
+++ test.py
@@ -1,2 +1,13 @@
-print 
+import json
+
+# Öffne die Datei
+with open('accounts.json', 'r') as f:
+    # Lade die JSON-Daten aus der Datei
+    data = json.load(f)
+
+ #print(data) 
+
+print(json.dumps(data, indent=10))
+
+
 
